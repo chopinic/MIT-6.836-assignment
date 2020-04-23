8d7743659be10dffb052051aadcc78b7a37d4157
chopinic <yangguangbus@163.com>
2020-04-24 02:35:18 +0800

    test

---
+++ assn3/starter3/CMakeFiles/a3.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/src/camera.cpp" "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/CMakeFiles/a3.dir/src/camera.cpp.o"
+  "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/src/clothsystem.cpp" "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/CMakeFiles/a3.dir/src/clothsystem.cpp.o"
+  "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/src/main.cpp" "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/CMakeFiles/a3.dir/src/main.cpp.o"
+  "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/src/particlesystem.cpp" "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/CMakeFiles/a3.dir/src/particlesystem.cpp.o"
+  "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/src/pendulumsystem.cpp" "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/CMakeFiles/a3.dir/src/pendulumsystem.cpp.o"
+  "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/src/simplesystem.cpp" "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/CMakeFiles/a3.dir/src/simplesystem.cpp.o"
+  "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/src/starter3_util.cpp" "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/CMakeFiles/a3.dir/src/starter3_util.cpp.o"
+  "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/src/timestepper.cpp" "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/CMakeFiles/a3.dir/src/timestepper.cpp.o"
+  "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/src/vertexrecorder.cpp" "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/CMakeFiles/a3.dir/src/vertexrecorder.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "vecmath/include"
+  "3rd_party/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/3rd_party/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/yangguang/archive/图形学/MIT-6.836-assignment/assn3/starter3/vecmath/CMakeFiles/vecmath.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
